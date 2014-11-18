717eff7ac817210551a4c78fc547897a7b0c2f3b
Zachary Ogren <zogren@oreilly.com>
2014-11-18 14:25:32 -0800

    removed admin from 2 routes

--- app/helpers/capturable_helper.rb
+++ app/helpers/capturable_helper.rb
@@ -1,6 +1,6 @@
 module CapturableHelper
-  def display_logout?
+  def display_logout?(resource_name)
     clean_url = request.original_url.split('?')[0]
-    [new_admin_session_url, admin_reset_password_url].exclude? clean_url
+    [new_session_url(resource_name), "#{root_url}/#{resource_name}/reset_password"].exclude? clean_url
   end
 end
