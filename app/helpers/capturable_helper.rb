3380249d9409a6cae674244770440b64a04014da
Aaron Sumner <asumner@oreilly.com>
2014-11-18 09:32:53 -0600

    WIP: Hard-code admin in Devise routes for testing

--- app/helpers/capturable_helper.rb
+++ app/helpers/capturable_helper.rb
@@ -1,6 +1,6 @@
 module CapturableHelper
   def display_logout?
     clean_url = request.original_url.split('?')[0]
-    [new_user_session_url, user_reset_password_url].exclude? clean_url
+    [new_admin_session_url, admin_reset_password_url].exclude? clean_url
   end
 end
